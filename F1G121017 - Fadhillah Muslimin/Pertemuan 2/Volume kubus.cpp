542315cfddf723cf46617cde5543fab0ef031091
unknown <fadhillahmuslimin03@gmail.com>
2021-10-18 05:44:36 +0700

    menambahkan file tugas 1

---
+++ F1G121017 - Fadhillah Muslimin/Pertemuan 2/Volume kubus.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	int sisi,hasil;
+	
+	cout<<"Sisi = ";
+	cin>>sisi;
+	hasil = sisi*sisi*sisi;
+	
+	cout<<"Volume = "<<hasil<<endl;
+	
+	return 0;
+}
