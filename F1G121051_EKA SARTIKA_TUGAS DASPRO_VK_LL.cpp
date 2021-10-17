273fba304554161d93521769089d886dd49f6b82
EkaSartika66 <92576682+EkaSartika66@users.noreply.github.com>
2021-10-17 12:05:24 +0800

    Add files via upload

---
+++ F1G121051_EKA SARTIKA_TUGAS DASPRO_VK_LL.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout<<"Nama        : EKA SARTIKA"<<endl;
+	cout<<"Nim         : F1G121051"<<endl;
+	cout<<"Jurusan     : ILMU KOMPUTER"<<endl;
+	cout<<"Mata Kuliah : DASAR-DASAR ALGORITMA PEMROGRAMAN"<<endl;
+	cout<<"................................................."<<endl;
+	
+    //Menghitung volume kubus//
+    
+    int sisi_1, sisi_2, sisi_3;
+
+	cout<<"Menghitung volume kubus"<<endl;
+	cout<<"Masukkan nilai sisi_1 = ";
+	cin>>sisi_1;
+	cout<<"Masukkan nilai sisi_2 = ";
+	cin>>sisi_2;
+	cout<<"Masukkan nilai sisi_3 = ";
+	cin>>sisi_3;
+	cout<<"Hasil volume kubus    = " <<sisi_1 * sisi_2 * sisi_3;
+	cout<<"cm^3"<<endl;
+	
+	//menghitung luas lingkaran
+	
+	float phi, r;
+	cout<<"                         "<<endl;
+	cout<<"Menghitung luas lingkaran"<<endl;
+	cout<<"Masukkan nilai phi   = ";
+	cin>>phi;
+	cout<<"Masukkan nilai r     = ";
+	cin>>r;
+	cout<<"Masukkan nilai r     = ";
+	cin>>r;
+	cout<<"Hasil luas lingkaran = " <<phi * r;
+	cout<<"cm^2";
+	
+	return 0;
+}
