0145bf1f8ad33abb8bd522fbc4123cb3a8a88d31
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-10-18 11:15:53 +0700

    menambahkan folder yang hilang

---
+++ F1G121021 - Ida Fadhillah As'adiyah/Pertemuan 2/Program Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float l,r;
+	const float phi = 3.14;
+	
+	cout << "================================="<<endl;
+	cout << "Program Menghitung Luas Lingkaran"<<endl;
+	cout << "================================="<<endl;
+	
+	cout << "Masukkan Jari-jari Lingkaran :";
+	cin>> r;
+	
+	l = phi*r*r;
+	
+	cout << "Luas Lingkaran Adalah : "<< l << endl;
+	
+	return 0;
+}
