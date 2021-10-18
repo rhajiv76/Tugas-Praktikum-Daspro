725b711d6f54bd15404bcb156974815fa201f59d
fen <nurfenti970@gmail.com>
2021-10-18 10:01:36 +0800

    menambahkan file tugas 1

---
+++ F1G121057-FENTI NUR/Pertemuan 2/Menentukan Luas Lingkaran.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+float r;
+
+int main()
+{
+	cout<<"Menentukan Luas Lingkaran"<<endl;
+	cout<<"Masukan Nilai Jari-jari : ";
+	cin>> r;
+	cout<<"Luas Lingkaran = "<< 3.14*r*r;
+	
+	return 0;
+	
+	
+	
+}
