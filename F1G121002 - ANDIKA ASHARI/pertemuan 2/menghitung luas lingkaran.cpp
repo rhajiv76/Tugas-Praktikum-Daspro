323bd6a49a7b007977b0261d9eae7a7b498002a2
Andika Ashari <ashariandika1404@gmail.com>
2021-10-18 00:52:31 -0700

    Menambahkan File Tugas 1

---
+++ F1G121002 - ANDIKA ASHARI/pertemuan 2/menghitung luas lingkaran.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	// menghitung luas lingkaran
+	float phi , r;
+	phi = 3.14;
+	r = 7.0;
+	cout<<phi * r * r;
+	
+	return 0;
+}
---
+++ F1G121002 - ANDIKA ASHARI/pertemuan 2/menghitung volume kubus.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//menghitung volume kubus
+	int p , t , l;
+	p = 2;
+	t = 4;
+	l = 5;
+	cout<<p * t * l;
+	
+	return 0;
+	
+}
