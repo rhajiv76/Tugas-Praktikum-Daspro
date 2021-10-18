d433081a243eecd24b2742dacc180090fb0a8bda
NARULHAYAT <fadhillahida@gmail.com>
2021-10-18 21:54:47 +0700

    menambahkan file tugas 1

---
+++ F1G121080 - Narul Hayat/Pertemuan 2/NarulHayat_F1G121080_LuasLingkaran.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+ int main(){
+ 	
+float phi=3.14,r;
+float luaslingkaran;
+ 	
+cout<<"\t+-------------------------------------------------+"<<endl;		
+cout<<"\t ||         ~~~~~~~Luas Lingkaran~~~~~~~       || "<<endl;
+cout<<"\t+-------------------------------------------------+"<<endl;
+cout<<"\t\t Nama  : Narul Hayat "<<endl;
+cout<<"\t\t NIM   : F1G121080 "<<endl;
+cout<<"\t+-------------------------------------------------+"<<endl;
+
+// awal program	
+cout<<"\tMasukan Jari-Jari       : ";cin>>r;
+luaslingkaran=phi*r*r;
+cout<<"\tLuas Lingkaranya Adalah : "<<luaslingkaran<<endl;
+cout<<"\t+-------------------------------------------------+"<<endl;
+//end program
+ 	
+ }
