7d7a408afc3a95d1ca158e5f723c0659bb15e187
fiki yamin <email_fikiyamin8@gmail.com>
2021-10-18 02:48:13 -0700

    menambahkan file tugas 1

---
+++ F1G121018 - FIKI YAMIN/Pertemuan2 pratikum daspro/menghitung volume kubus-fiki yamin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s,volume;
+
+    cout<<"masukan sisi kubus : ";
+    cin>>s;
+
+    volume=s*s*s;
+    cout<<"volume kubus adalah "<<volume;
+    return 0;
+
+}
