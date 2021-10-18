6a694add5815089b05daa61267bf4570c5358edf
AnggunPratiwi <anggunpratiwiii081@gmail.com>
2021-10-18 16:20:19 +0800

    menambahkan file tugas 1

---
+++ F1G121039- anggun pratiwi/Petemuan 2/anggun 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//menghitung volume kubus 
+	int p, t, l;
+	p = 4;
+	t = 3;
+	l = 7;
+	cout<<p * t * l;
+	
+	return 0;
+	
+}
---
+++ F1G121039- anggun pratiwi/Petemuan 2/anggun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	//menghitung luas lingkaran 
+	
+	float phi, r;
+	phi = 3.14;
+	r= 8.0;
+	
+	cout<<phi * r * r;
+	
+	return 0;
+}
