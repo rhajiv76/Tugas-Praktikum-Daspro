0b29735a2d9d354dc4cbdeb7ca8e1c6257ac1946
HP <muhammadazeslimrajes0178@gmail.com>
2021-10-18 14:04:24 +0700

    menambahkan file tugas 1

---
+++ F1G121016 - Azeslim/pertemuan 2/rumus menentukan luas lingkaran.cpp
@@ -0,0 +1,21 @@
+//		NAMA	: AZESLIM
+//		NIM 	: F1G1210167
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  float a,b,c,hasil;
+  b = 3.14;
+  cout<<"masukkan nilai jari-jari =";
+  cin>>a;
+  c = a * a;
+  cout<<"r² = "<<c<<endl;
+  cout<<"diketahui "<<b<<"×"<<c<<endl;
+  hasil = b * c;
+  cout<<"untuk hasil luas lingkaran = "<<hasil<<"cm²";
+  
+  
+  
+  
+}
---
+++ F1G121016 - Azeslim/pertemuan 2/rumus volume kubus.cpp
@@ -0,0 +1,17 @@
+//		NAMA	: AZESLIM
+//		NIM		: F1G121016
+#include <iostream>
+using namespace std;
+int main(){
+    int s,volume;
+
+    cout<<"masukan sisi kubus : ";
+    cin>>s;
+
+    volume=s*s*s;
+    cout<<"volume kubus adalah "<<volume;
+    return 0;
+
+}
+
+
