181106eb7f8cf8cc358028691fb462a4c62c1b7a
imadealitdwisaputra <madealit211003@gmail.com>
2021-10-18 02:03:28 -0700

    menambahkan file tugas 1

---
+++ F1G121061 - I Made Alit Dwi Saputra/Pertemuan 2/luas lingkaran.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "i made alit dwi saputra\n";
+
+  float a,b,c,hasil;
+  b = 3.14;
+  cout<<"masukkan nilai jari-jari =";
+  cin>>a;
+  c = a * a;
+  cout<<"r² = "<<c<<endl;
+  cout<<"diketahui "<<b<<"×"<<c<<endl;
+  hasil = b * c;
+  cout<<"untuk hasil luas lingkaran = "<<hasil<<"cm²";
+  
+  
+  
+  
+}
