8e0581dd7d9802817d212e5cc48ec279a29b6975
FEBRIANI769 <Unifebriani769@gmail.com>
2021-10-18 22:21:54 +0800

    menambahkan file tugas 1

---
+++ F1G121056 - Febriani/Pertemuan 2/Tugas 1/daspro volume kubus.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//menghitung volume kubus
+int main(){
+	
+	
+	cout<< " TUGAS DASPRO" << endl;
+	cout<< " MENGHITUNG VOLUME KUBUS MENGUNAKAN DEV C++" <<endl;
+	cout<< " NAMA: FEBRIANI" << endl;
+	cout<< " NIM: F1G121056" << endl;
+	
+	
+    int s,volume;
+    string dimensi;
+
+    cout<< " masukan sisi kubus : ";
+    cin>>s;
+    
+
+    volume=s*s*s; // rumus volume kubus
+    cout<< " volume kubus adalah "<<volume;
+    return 0;
+
+} 
