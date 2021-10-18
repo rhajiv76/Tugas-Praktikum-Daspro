fa04081bd3c104f9c7da0db5754fee7863c35c6a
Lynx-riel <adventerous84@gmail.com>
2021-10-18 08:00:41 +0800

    menambahkan file tugas 1

---
+++ F1G121023 - Lily Firdaus Gusrin/Pertemuan 2/tugas 1/menentukan volume kubus.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+cout<<"Nama         = Lily Firdaus Gusrin"<<endl;
+cout<<"NIM          = F1G121023"<<endl;
+cout<<"Jurusan      = Ilmu Komputer"<<endl;
+cout<<"Fakultas     = FMIPA"<<endl;
+cout<<endl;
+
+    float s, v;
+    cout<<"masukkan panjang sisi  1 (cm)= ";cin>>s;
+
+    cout<<endl;
+    cout<<"==============================="<<endl;
+    cout<<"Diketahui panjang sisi 1 = "<<s<<"cm"<<endl;
+    cout<<"==============================="<<endl;
+    cout<<endl;
+
+    v=s*s*s;
+    cout<<"volume kubus = s^3"<<endl;
+    cout<<"volume kubus = "<<v<<"cm^3";
+
+    return 0;
+
+}
