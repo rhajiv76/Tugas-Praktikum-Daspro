8813edfb13a70c74557788d9e30e3f147d939773
Fadlilaode <92584424+Fadlilaode@users.noreply.github.com>
2021-10-18 19:59:31 +0800

    Add files via upload

---
+++ daspro volume kubuss.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//menghitung volume kubus
+int main(){
+	
+	
+	cout<< " TUGAS DASPRO" << endl;
+	cout<< " MENGHITUNG VOLUME KUBUS MENGUNAKAN DEV C++" <<endl;
+	cout<< " NAMA: fadli la ode mardin " << endl;
+	cout<< " NIM: F1G121054" << endl;
+	
+	cout<< " silakan ikuti langkah berikut" << endl;
+	
+    int s,volume;
+    string dimensi;
+
+    cout<< " masukan sisi kubus : ";
+    cin>>s;
+    
+
+    volume=s*s*s; // rumus volume kubus
+    cout<< " volume kubus adalah "<<volume;
+    return 0;
+
+} 
