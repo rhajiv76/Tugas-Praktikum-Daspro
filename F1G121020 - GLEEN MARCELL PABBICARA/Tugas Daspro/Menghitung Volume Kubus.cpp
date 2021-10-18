1201864a9c5cb7991c70096a97d9a85bfeb5f121
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-10-18 11:17:44 +0700

    mengubah rar menjadi folder

---
+++ F1G121020 - GLEEN MARCELL PABBICARA/Tugas Daspro/Menghitung Volume Kubus.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+cout << "|Tugas Daspro Menghitung Volume Kubus Oleh;|\n";
+    cout << "Nama : Gleen Marcell Pabbicara\n";
+    cout << "Nim : F1G121020\n";
+    cout << "\n";
+    int s,volume;
+
+    cout<<"masukan sisi kubus : ";
+    cin>>s;
+
+    volume=s*s*s;
+    cout<<"volume kubus adalah "<<volume;
+    return 0;
+
+}
