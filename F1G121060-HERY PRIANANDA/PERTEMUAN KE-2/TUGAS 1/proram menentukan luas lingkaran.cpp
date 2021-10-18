e94bd41f146ece177bd88cfae0cbe44097b53801
HERYPRIANANDA <herypriananda@gmail.com>
2021-10-18 14:54:01 +0700

    menambahkan file tugas 1

---
+++ F1G121060-HERY PRIANANDA/PERTEMUAN KE-2/TUGAS 1/proram menentukan luas lingkaran.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+  {
+    cout << "|Tugas Daspro Menghitung Luas Lingkaran Oleh;|\n";
+    cout << "Nama : Hery Priananda\n";
+    cout << "Nim : F1G121060\n";
+    cout << "\n";
+    float luas, phi=3.14;
+    int r;
+    cout<<"Masukan Jari-jari : ";
+    cin>>r;
+    
+    luas=phi*r*r;
+
+    cout<<"Luas Lingkaran = "<<luas<<endl;
+  getch();
+  }
