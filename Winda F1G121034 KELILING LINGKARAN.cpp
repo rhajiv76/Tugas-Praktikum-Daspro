31803831a2a2e42fabf78274a7d78e60c398e4c2
Winda270703 <92614515+Winda270703@users.noreply.github.com>
2021-10-18 17:17:42 +0800

    F1G121034-WINDA

---
+++ Winda F1G121034 KELILING LINGKARAN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	std::cout<<"Winda\n:";
+	
+	float luas, phi=3.14;
+	int r;
+	cout<<"Masukkan Jari-Jari : ";
+	cin>>r;
+	
+	luas=phi*r*r;
+	
+	cout<<"Luas Lingkaran : "<<luas<<endl;
+	
+	
+	return 0;
+	
+	
+}
