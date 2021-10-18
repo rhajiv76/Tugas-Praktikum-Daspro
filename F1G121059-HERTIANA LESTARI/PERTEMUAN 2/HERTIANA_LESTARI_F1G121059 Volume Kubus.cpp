463590c06156c617d40e74f3bb59b2f20514e2e2
Hertiana Lestari <hertianalestari2@gmail.com>
2021-10-18 13:44:16 +0700

    menambahkan file tugas 1

---
+++ F1G121059-HERTIANA LESTARI/PERTEMUAN 2/HERTIANA_LESTARI_F1G121059 Volume Kubus.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "Nama     : Hertiana Lestari" << endl;
+	cout << "stambuk  : F1G121059" << endl;
+	cout << "jurusan  : Ilmu Komputer" << endl;
+	cout << "Fakultas : Matematika dan Ilmu Pengetahuan Alam" << endl;
+	
+	//menghitung volume kubus
+	int p , t , l;
+	p = 4;
+	t = 8;
+	l = 10;
+	cout<<p * t * l;
+	
+	return 0;
+	
+}
