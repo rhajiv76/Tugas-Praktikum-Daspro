64abb760d90249578c60bf973f338e131065c314
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-10-18 11:03:25 +0700

    menambahkan beberapa file yang hilang dari repo

---
+++ F1G121017 - Fadhillah Muslimin/Pertemuan 2/Luas Lingkaran.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	float phi,luas;
+	int r;
+	phi = 3.14;
+	cout<< "Masukkan Jari- Jari = ";
+	cin>>r;
+	
+	luas = phi*r*r;
+
+	cout<<"Luas Lingkaran = "<<luas<<endl;
+	
+	return 0;
+}
