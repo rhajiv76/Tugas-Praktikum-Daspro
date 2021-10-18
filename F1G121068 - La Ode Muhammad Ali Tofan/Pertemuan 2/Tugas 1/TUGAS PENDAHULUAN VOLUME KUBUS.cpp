1a8707a890da7b96af6282354a140faf6e726b00
Lynx-riel <alitofan2325@gmail.com>
2021-10-18 19:33:47 +0800

    menambahkan file tugas 1

---
+++ F1G121068 - La Ode Muhammad Ali Tofan/Pertemuan 2/Tugas 1/TUGAS PENDAHULUAN VOLUME KUBUS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+	using namespace std;
+	
+int main(){
+	
+	int sisi1; 
+	int	sisi2;
+	int	sisi3;
+	int	volumekubus;
+	
+	
+cout <<"menghitung volume kubus"<<endl;
+
+//peyelsaiyan
+
+cout <<"masukan sisi1="<<endl;
+cin>> sisi1;
+cout <<"masukan sisi2="<<endl;
+cin>> sisi2;
+cout <<"masukan sisi3="<<endl;
+cin>> sisi3;
+volumekubus= sisi1*sisi2*sisi3;
+cout <<"volume kubus :"<<volumekubus<<endl;
+
+return 0;
+
+}
