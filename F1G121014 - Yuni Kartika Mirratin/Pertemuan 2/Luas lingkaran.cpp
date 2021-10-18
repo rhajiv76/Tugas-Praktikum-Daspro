35accf35e5f6a806fed839fe2cf84c2c67ef329c
yun1127 <92589231+yun1127@users.noreply.github.com>
2021-10-18 12:46:28 -0700

    Menambahkan file tugas 1

---
+++ F1G121014 - Yuni Kartika Mirratin/Pertemuan 2/Luas lingkaran.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "Yuni Kartika Mirratin\n";
+  std::cout << "Menghitung luas lingkaran\n";
+
+  float a,b,c,hasil;
+  b = 3.14;
+  cout<<"masukkan nilai jari-jari =";
+  cin>>a;
+  c = a * a;
+  cout<<"r² = "<<c<<endl;
+  cout<<"diketahui "<<b<<"×"<<c<<endl;
+  hasil = b * c;
+  cout<<" luas lingkaran = "<<hasil<<"cm²";
+  
+  
+  
+  
+}
---
+++ F1G121014 - Yuni Kartika Mirratin/Pertemuan 2/Volume Kubus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	
+	std::cout << "Yuni Kartika Mirratin\n";
+  std::cout << "Menghitung Volume Kubus\n";
+    //mencari rumus volume kubus
+    //sisi x sisi x sisi
+   
+
+  int sisi_1,hasil;
+    
+    cout<<"masukkan nilai sisi kubus = ";
+      cin>>sisi_1;
+  hasil = sisi_1 * sisi_1 * sisi_1;
+  cout<<"\n";
+  cout<<"untuk hasil volume dari kubus adalah sisi x sisi x sisi = "<<hasil<<"cm³";
+  
+}
