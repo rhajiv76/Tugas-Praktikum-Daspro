092e9eb7fd4c9fe7dbbd102f62401f023033d1d7
Siskaamaliapratiwihanifa <siskamoniampe@gmail.com>
2021-10-18 22:45:43 +0800

    menambahkan file tugas 1

---
+++ F1G121010 - Siska Amalia/pertemuan 2/tugas 1/F1G121010 tugas daspro luas lingkaran.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	float luas, phi=3.14;
+	int r;
+	
+	cout<<"masukkan jari-jari : ";
+	cin>>r;
+	
+	luas=phi*r*r;
+	
+	cout<<"luas lingkaran = "<<luas<<endl;
+	
+		
+}
