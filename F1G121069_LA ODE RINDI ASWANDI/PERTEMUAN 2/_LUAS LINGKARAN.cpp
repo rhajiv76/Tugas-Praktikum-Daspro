292526a1df4588f8ec27e208d7d2c8cf5051afe1
laoderindiaswandi <laoderindiaswandi@gmail.com>
2021-10-18 21:58:10 +0800

    menambahkan file tugas 1

---
+++ F1G121069_LA ODE RINDI ASWANDI/PERTEMUAN 2/_LUAS LINGKARAN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "LA ODE RINDI ASWANDI\n";
+
+  float a,b,c,hasil;
+  b = 3.14;
+  cout<<"masukkan nilai jari-jari =";
+  cin>>a;
+  c = a * a;
+  cout<<"r² = "<<c<<endl;
+  cout<<"diketahui "<<b<<"x"<<c<<endl;
+  hasil = b * c;
+  cout<<"untuk hasil luas lingkaran = "<<hasil<<"cm²";
+  
+  
+  
+  
+}
