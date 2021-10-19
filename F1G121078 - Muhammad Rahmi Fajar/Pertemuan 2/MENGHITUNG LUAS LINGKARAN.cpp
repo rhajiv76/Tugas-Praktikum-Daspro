e1b60a982664a8d6c5d4fe7392ed36141a4d9177
Fajarrahmi666 <muhammadrahmifajar69@gmail.com>
2021-10-18 19:13:53 -0700

    menambahkan tugas 1

---
+++ F1G121078 - Muhammad Rahmi Fajar/Pertemuan 2/MENGHITUNG LUAS LINGKARAN.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main(){
+ 	
+ 	float phi=3.14;
+ 	float r;
+ 	float luaslingkaran;
+ 	
+ 	cout<<"menghitung luas lingkaran"<<endl;
+ 	cout<<"========================"<<endl;
+ 	
+ 	//penyelesaiyan
+ 	
+ 	cout<<"masukan jari jari ="<<endl;
+ 	cin >>r;
+ 	luaslingkaran=phi*r*r;
+ 	cout<<"luas lingkaranya adalah ="<<luaslingkaran<<endl;
+ 	cin >>luaslingkaran;
+ 	
+ 	return 0;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
