43e3159c507d5b0a73631df3233de6994a008f5b
unknown <you@example.com>
2021-10-19 09:35:26 -0700

    menambahkan file tugas 1

---
+++ F1G121046 - Desti Nur Amaliah/Pertemuan 2/Luas Lingkaran desti nur amaliah.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+using namespace std;
+int main(){
+	cout<<"Desti nur amaliah" << endl ;
+	cout<<"F1G121046" << endl ;
+  float l, r;
+  const float phi = 3.14;
+
+  cout << "Program Menghitung Luas Lingkaran\n";
+
+  cout << "Masukkan panjang jari-jari lingkaran: ";
+  cin >> r;
+  l = phi*r*r;
+  cout << "Luas Lingkaran adalah "<< l << endl;
+  return 0;
+}
---
+++ F1G121046 - Desti Nur Amaliah/Pertemuan 2/Volume kubus desti nur amaliah.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout<<"desti nur amaliah" << endl;
+	cout<<"F1G121046" << endl;
+	int s, volume ;
+	cout<<"masukkan sisi kubus : ";
+	cin>>s;
+	volume=s*s*s;
+	cout<<"volume kubus adalah"<<volume;
+
+return 0;
+}
