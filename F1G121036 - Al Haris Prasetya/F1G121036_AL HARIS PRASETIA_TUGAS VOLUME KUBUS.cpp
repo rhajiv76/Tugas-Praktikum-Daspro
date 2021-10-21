788b580e93e5419be8426e514fd6e701808aa74a
Panggilsajaabay <akbarwahidamir@gmail.com>
2021-10-21 17:50:34 +0700

    menambahkan tugas

---
+++ F1G121036 - Al Haris Prasetya/F1G121036_AL HARIS PRASETIA_TUGAS VOLUME KUBUS.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	
+	//mencari rumus kubus
+	//sisi x sisi x sisi
+	
+	int sisi, hasil;
+	
+	cout<<"sisi kubus = ";
+	cin>>sisi;
+	hasil= sisi * sisi * sisi;
+	cout<<"volume kubus adalah "<<hasil<<"m^2";
+	
+	return 0;
+	
+}
+
