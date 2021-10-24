2cefb767ceecb24acc0bf5073f9d7a0dd72d08c0
Niken Mutiara Wulandari <92593717+nikenmutiara@users.noreply.github.com>
2021-10-24 23:53:06 +0700

    menambahkan tugas file 2

--- F1G121027 - Niken Mutiara Wulandari/Pertemuan 3/cek password.cpp.cpp
+++ F1G121027 - Niken Mutiara Wulandari/Pertemuan 3/cek password.cpp.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
-#include <string>
+#include<string>
 
 using namespace std;
 
-int main (){
+int main(){
 	
-	string username = "";
-	string password = "";
-	bool loginSuccess = false;
+	string nama, pass, username, password;
+	bool LoginSuccess = false;
+
+
+//halaman registrasi
+do {
+	cout<<"==========registrasi=========="<<endl;
+	cout<<"silahkan buat akun anda sebelum bisa login"<<endl;
+	cout<<"Username =  ";
+	getline(cin,nama);
+	cout<<"Password =  ";
+	getline(cin,pass);
+	cout<<"akun anda telah dibuat, silahkan melakukan Login.";
+	cout<<endl;
+
+//halaman login
+	login:
+	cout<<"==========Login=========="<<endl;
+	cout<<"username: ";
+	getline(cin,username);
+	cout<<"password: ";
+	getline(cin,password);
+	cout<<endl;
 	
-	cout << "\tHai! silahkan Log in dibawah.\n\n";
+	if (username==nama && password==pass){
+		cout << "\nHalo\n\n"<<nama<<endl;
+		LoginSuccess = true;
+	}else{
+		cout<<"username/password yang anda masukan salah"<<endl;
+		cout<<"silahkan registrasi ulang"<<endl;
+		}
 	
-	do{
-		cout << "username: ";
-		cin >> username;
-		cout << "password: ";
-		cin >> password;
-		
-		if(username == "Niken" && password == "nikenmw"){
-			cout << "\nHalo Niken Mutiara\n\n";
-			loginSuccess = true;
-			}
-		else{
-			cout << "username dan Password salah \n";
-			cout << "silahkan coba lagi.";
-			} 
-		
-		} while(!loginSuccess);
-		
-	system("pause");
-	return 0;	
-	}
+}while(!LoginSuccess);
+
+system("pause");
+return 0;		
+}
+			
+
+
+
+
 
